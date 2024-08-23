8e946b57e342c2834edaa5fe44000ec3860127c9
mudmykola <mykola.mud@gmail.com>
2024-08-23 11:24:46 +0300

    settings(App): project configuration 1.0

---
+++ src/components/home/HomeComponent.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="home-inner">Home</div>
+</template>
+
+<style scoped></style>
---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,27 @@
+<script setup>
+import NavigationComponent from '@/components/navigation/NavigationComponent.vue';
+import HeaderComponent from '../components/header/HeaderComponent.vue';
+</script>
+
+<template>
+  <div class="default-layout">
+    <header class="header">
+      <HeaderComponent />
+    </header>
+    <main class="main">
+      <aside class="main-aside">
+        <NavigationComponent />
+      </aside>
+      <div class="main-content">
+        <slot />
+      </div>
+    </main>
+    <footer class="footer">footer</footer>
+  </div>
+</template>
+
+<style lang="scss">
+.main {
+  display: flex;
+}
+</style>
